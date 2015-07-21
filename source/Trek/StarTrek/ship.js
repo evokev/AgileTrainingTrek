f8d67911e34e52f618b142eff591ea68161cb0e8
Kevin Carter <kevin.carter@anthem.com>
2015-07-21 17:38:27 -0400

    updates to ship testing and subsystems

--- source/Trek/StarTrek/ship.js
+++ source/Trek/StarTrek/ship.js
@@ -4,9 +4,15 @@ Ship = function () {
     "use strict";
     this.name = "FantasyFive Ship";
     this.isSubsystemDamaged = false;
+
+    this.isNearStarbase = false;
     
     this.shield = new Shield();
-    
+
+    this.subsystems = {};
+    this.subsystems['phasers'] = new Subsystem("phasers",300,1);
+    this.subsystems['shields'] = new Subsystem("shields",500,1);
+    this.subsystems['engine'] = new Subsystem("engine",200,1);
 };
 
 Ship.prototype = {
@@ -14,12 +20,38 @@ Ship.prototype = {
         "use strict";
         return this.name;
     },
-    
+
     makeReadyForBattle: function () {
         "use strict";
         this.shield.raise();
     },
-    
+
+    rest: function () {
+        "use strict";
+        //this.isResting = true;
+
+        // Heal subsystems this turn.  All subsystems are repaired by a factor
+        this.subsystems["phasers"].rest();
+        this.subsystems["shields"].rest();
+        this.subsystems["engine"].rest();
+    },
+
+    goToStarbase: function () {
+        "use strict";
+        this.isNearStarbase = true;
+    },
+
+    leaveStarbase: function () {
+        "use strict";
+        this.isNearStarbase = false;
+    },
+
+    damageSubsystem: function (subsystemAffected, amount) {
+        "use strict";
+
+        this.subsystems[subsystemAffected].getDamaged(amount);
+    },
+
     getHit: function (damageAmount) {
         "use strict";
         var amountLeftOver = 0;
@@ -28,6 +60,9 @@ Ship.prototype = {
         if (amountLeftOver > 0) {
             this.isSubsystemDamaged = true;
             // figure out which subsystem is damaged and apply damage
+            
+            var subsystemAffected = 'phasers';  //Math.floor((Math.random() * 3) + 1);
+            this.damageSubsystem(subsystemAffected,amountLeftOver);
         }
     }
 
--- source/Trek/Tests/shipCharacterization.test.js
+++ source/Trek/Tests/shipCharacterization.test.js
@@ -5,12 +5,49 @@ describe("ship", function() {
         ship = new Ship();
     });
 
+    describe("defaults", function() {
 
-    it("verify that ship exists and is named FantasyFive Ship", function() {
-        var shipName = ship.getName();
+        it("verify that ship exists and is named FantasyFive Ship", function() {
+            var shipName = ship.getName();
+
+            expect(shipName).toBe("FantasyFive Ship");
+        });
+
+        it("has subsystems", function() {
+            expect(Object.keys(ship.subsystems).length).toBe(3);
+        });
+
+        it("first subsystem is phasers and has a capacity of 300", function() {
+            expect(ship.subsystems['phasers'].type).toBe("phasers");
+            expect(ship.subsystems['phasers'].damageCapability).toBe(300);
+            expect(ship.subsystems['phasers'].unitOfReapir).toBe(1);
+            expect(ship.subsystems['phasers'].canOperate).toBe(true);
+
+        });
         
-        expect(shipName).toBe("FantasyFive Ship");
+        it("first subsystem is shields and has a capacity of 500", function() {
+            expect(ship.subsystems['shields'].type).toBe("shields");
+            expect(ship.subsystems['shields'].damageCapability).toBe(500);
+            expect(ship.subsystems['shields'].unitOfReapir).toBe(1);
+            expect(ship.subsystems['shields'].canOperate).toBe(true);
+        });
+        
+        it("first subsystem is warp engine and has a capacity of 200", function() {
+            expect(ship.subsystems['engine'].type).toBe("engine");
+            expect(ship.subsystems['engine'].damageCapability).toBe(200);
+            expect(ship.subsystems['engine'].unitOfReapir).toBe(1);
+            expect(ship.subsystems['engine'].canOperate).toBe(true);
+        });
+        
 
+        
+        it("not damaged by default", function() {
+            expect(ship.isSubsystemDamaged).toBe(false);
+        });
+    
+        it("is not near a starbase by default", function() {
+            expect(ship.isNearStarbase).toBe(false);
+        });
     });
 
     it("verify ship is ready for battle", function() {
@@ -21,6 +58,19 @@ describe("ship", function() {
         expect(ship.shield.energy).toBe(5000);
     
     });
+
+    it("can go near a starbase", function() {
+        ship.goToStarbase();
+
+        expect(ship.isNearStarbase).toBe(true);
+    });
+
+    it("can leave a starbase", function() {
+        ship.leaveStarbase();
+
+        expect(ship.isNearStarbase).toBe(false);
+    });
+    
     
       describe("damage within shield capacity", function() {
         var damageAmount;
@@ -76,8 +126,29 @@ describe("ship", function() {
         });
                     
       });
-        
 
-    
+    it("can take 300 damage to phasers and become inoperable", function() {
+        ship.damageSubsystem('phasers',300);
+        
+        expect(ship.subsystems['phasers'].isOperable()).toBe(false);
+    });
+
+    it("can take less than 300 damage to phasers and are still usable", function() {
+        ship.damageSubsystem('phasers',299);
+        
+        expect(ship.subsystems['phasers'].isOperable()).toBe(true);
+    });
+
+    it("can take 500 damage to shields and become inoperable", function() {
+        ship.damageSubsystem('shields',500);
+        
+        expect(ship.subsystems['shields'].isOperable()).toBe(false);
+    });
+
+    it("can take less than 500 damage to shields and are still usable", function() {
+        ship.damageSubsystem('shields',499);
+        
+        expect(ship.subsystems['shields'].isOperable()).toBe(true);
+    });
     
 });
